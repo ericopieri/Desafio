e9cc71d7c5eba7b0fc687e2c54179f5ac7adeea1
ericopieri <erico.pieri@gmail.com>
2023-03-02 10:44:14 -0300

    post produto 100%

--- produtoPost.php
+++ produtoPost.php
@@ -1,17 +1,17 @@
 <?php
 require_once "db.php";
+require_once "headers.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nome = $_POST['nome'];
     $valor = $_POST['valor'];
     $tipo = $_POST['tipo'];
 
-    $query = "INSERT INTO produto (nome, valor, tipo) VALUES (?, ?, ?)";
-    $insertProduto = $pdo->prepare($query);
-    $insertProduto->execute([$nome, $valor, $tipo]);
+    $insertProduto = $pdo->prepare("INSERT INTO produto (nome, valor, tipo) VALUES ('$nome', '$valor', '$tipo')");
+    $insertProduto->execute();
 
     if ($insertProduto->rowCount() > 0) {
-        echo "Inserção realizada com sucesso!";
+        require_once "produtos.php";
     } else {
         $erro = $insertProduto->errorInfo();
         echo "Erro ao inserir o produto: " . $erro[2];
